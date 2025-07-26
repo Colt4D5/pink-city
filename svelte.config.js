9a153342131cb50d7c7185669e63e8ae4f12bc19
Colton Arthur Allen <allen.colton@hotmail.com>
2025-07-26 14:00:32 -0700

    configure for netlify

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,16 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+      // if true, will create a Netlify Edge Function rather
+      // than using standard Node-based functions
+      edge: false,
+  
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app.
+      // if `edge` is true, this option cannot be used
+      split: false
+    }),
     alias: {
       $assets: "src/lib/assets",
       $components: "src/lib/components",
